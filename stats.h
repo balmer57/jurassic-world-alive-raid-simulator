e28974cfd241b195056364723e4f8b60094cb002
Balmer <balmer57@gmail.com>
2022-11-17 17:38:29 +0300

    Round stats

--- stats.h
+++ stats.h
@@ -16,18 +16,27 @@ class Stats
 	static int out_of_turns_count;
 	static std::vector< std::vector<int> > death_count;
 	static std::vector< std::vector<int> > min_hp;
+	static std::vector<int> min_turns_count;
+	static std::vector<int> max_turns_count;
 	static int current_turn;
+	static int current_turn_in_round;
+	static int current_round;
 	static std::vector<bool> was_killed_this_turn;
+	static bool stats_inited;
+
 public:
-	static void Init(int team_size, int turns_count);
+	static void Init(int team_size, int turns_count, int round_count);
 	static int GetWinCount() {return win_count;}
 	static int GetDefeatCount() {return defeat_count;}
 	static int GetOutOfTurnsCount() {return out_of_turns_count;}
-	static std::vector< int > GetMinHP(int i) {return min_hp[i];}
+	static std::vector<int> GetMinHP(int i) {return min_hp[i];}
 	static std::vector<int> GetDeaths(int i) {return death_count[i];}
+	static std::vector<int> GetMinTurnCount() {return min_turns_count;}
+	static std::vector<int> GetMaxTurnCount() {return max_turns_count;}
 
 	static void ResetTurn();
 	static void NextTurn();
+	static void NextRound();
 	static void RegisterHit(int target, int hp);
 	static void RegisterResult(BattleResult result);
 };
